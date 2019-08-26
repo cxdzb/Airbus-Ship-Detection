c567feeadd2d08f5c8e8063c6f991801d74e9be6
cxdzb <1005968086@qq.com>
2019-08-26 17:54:11 +0800

    Enlarge the number of the epoch.(Get the 5_32_20.h5.)

---
+++ liushupei/model_detect.py
@@ -0,0 +1,25 @@
+from liushupei.preprocess.data_deal import rle_to_array, array_to_image
+from PIL import Image
+import pandas as pd
+from keras.models import load_model
+from keras import backend as K
+import os
+
+
+def IoU(y_true, y_pred, eps=1e-6):
+    if K.max(y_true) == 0.0:
+        return IoU(1 - y_true, 1 - y_pred)
+    intersection = K.sum(y_true * y_pred, axis=[1, 2, 3])
+    union = K.sum(y_true, axis=[1, 2, 3]) + K.sum(y_pred, axis=[1, 2, 3]) - intersection
+    return 1 - K.mean((intersection + eps) / (union + eps), axis=0)
+
+
+model = load_model("5_32_20.h5", custom_objects={'IoU': IoU})
+
+results = pd.read_csv(r"E:\DataSet\airbus-ship-detection\segmentations.csv")
+x, y_true = rle_to_array(Image.open(os.getcwd() + "\\data\\0af984fcd.jpg"),
+                         results["EncodedPixels"][results["ImageId"] == "0af984fcd.jpg"])
+x = x.reshape(1, 768, 768, 3) / 255
+y_pre = model.predict(x)[0]
+array_to_image(y_true * 255, (768, 768))
+array_to_image(y_pre * 255, (768, 768))
--- liushupei/model_run.py
+++ liushupei/model_run.py
@@ -1,21 +1,7 @@
 from liushupei.preprocess.data_generator import generator
+from liushupei.model.model_assess import draw_loss
 import pandas as pd
 import os
-
-# filenames = os.listdir(os.getcwd() + "\\data")[1:2]
-# results = pd.read_csv(os.getcwd() + "\\data\\data.csv")
-#
-# X, Y = [], []
-# for filename in filenames:
-#     img = Image.open(os.getcwd() + "\\data\\" + filename)
-#     mask = results["EncodedPixels"][results["ImageId"] == filename].values[0]
-#     x, y = rle_to_array(img, mask)
-#     X.append(x)
-#     Y.append(y)
-# X,Y=np.asarray(X),np.asarray(Y)
-# from sklearn.model_selection import train_test_split as tts
-#
-# x_train, x_test, y_train, y_test = tts(X, Y, test_size=0.2, random_state=1)
 
 imgs = os.listdir(r"E:\DataSet\airbus-ship-detection\ship")
 results = pd.read_csv(r"E:\DataSet\airbus-ship-detection\segmentations.csv")
@@ -27,16 +13,8 @@ from liushupei.model.model_build import Unet
 
 model = Unet(input_shape=(768, 768, 3))
 
-model.fit_generator(train_gen, steps_per_epoch=32, epochs=5, validation_data=val_gen, validation_steps=1)
-model.save("5_32_5.h5")
+history = model.fit_generator(train_gen, steps_per_epoch=32, epochs=30, validation_data=val_gen,
+                              validation_steps=1).history
+model.save("5_32_30.h5")
 
-from liushupei.preprocess.data_deal import rle_to_array, array_to_image
-from PIL import Image
-
-x, y_true = rle_to_array(Image.open(os.getcwd() + "\\data\\0af984fcd.jpg"),
-                         results["EncodedPixels"][results["ImageId"] == "0af984fcd.jpg"])
-x = x.reshape(1, 768, 768, 3) / 255
-y_pre = model.predict(x)[0]
-print(y_true.shape)
-array_to_image(y_true * 255, (768, 768))
-array_to_image(y_pre * 255, (768, 768))
+draw_loss(history["loss"], history["val_loss"])
